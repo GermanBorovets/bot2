e667b959d5cb036a4ef608bde090a54abc6ad253
Герман <82041544+GermanBorovets@users.noreply.github.com>
2025-06-03 09:57:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ def download():
 
 
 
-if __name__ == '__main__':
-    port=8080
-    app.run(debug=True, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = 8080
+    app.run(debug=True,host='0.0.0.0',port=port)
     
